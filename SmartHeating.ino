d4b122d5066e944f6acee84dd2f93addd372c243
RadovanKrejci <97411330+RadovanKrejci@users.noreply.github.com>
2022-04-10 12:44:03 +0200

    Initial version
    First version. Control over the keyboard and 2x16 display. No remote yet.

---
+++ SmartHeating.ino
@@ -0,0 +1,441 @@
+/* Smart control of fireplace stove and regular heater in one house. 
+ Main features are:
+  1) Safety - should the water from fireplace be too hot it turns on the pump to cool it down and if it reaches critical threshold an alarm turns on
+  2) Thermostat - turns heater on / off as needed to combine the power with fireplace stove and raeach preset temperature.
+      Thermostat uses two different sensors for two different scenarios
+    a) Winter / antifreeze - sensor that is placed close to plumbing is used to maintain low temperature but avoid the water to freeze (5 degrees)
+    b) Normal / Summer - sensor placed in the main room is used to maintaing comfortable temperature (e.g. 22 degrees)
+  3) Other 
+      - the pump as well as main heater are turned on / off with preset delays to avoid their damage in case of frequent switches.
+      - the entire system can easily be configured and status checked in 2x16 display
+      - some other small things. 
+  Language: menu items are in czech. 
+*/
+#include <LiquidCrystal.h>
+//#include <OneWire.h> // zakomentovano abych na pinech A4,A5 nemel I2C SDA a SCL protokol a mohl to pouzit pro relatka
+#include <DallasTemperature.h>
+#include <DHT.h>
+#include <EEPROM.h>
+#include "Menu.h"
+
+#define VERSION 3 //  increasing the version number will force the replacement of EEPROM content by the defaults
+#define VPOSITION 512 // position in EEPROM to store version number. Must be higher then SETTINGSMENUSIZE 
+
+enum menu_type {
+  MAIN_MENU,
+  SETTINGS_MENU
+};
+
+enum SettingMenuItem {
+  SETROOM1TEMP = 0,
+  SETROOM2TEMP,
+  SETANTIFREEZE,
+  SETMAXWATERTEMP,
+  SETELHEATER,
+  SETPUMP,
+  SETHYSTER,
+  BACK,
+  SETTINGSMENUSIZE
+};
+
+#define MINTEMP 0
+
+item_t SETTINGSMENU[SETTINGSMENUSIZE] =  {
+  {"Teplota Patro   ", DEFAULT_ROOM1_TEMP, TEMPSTEP, 10000, MINTEMP, " \xDF""C"},
+  {"Teplota Prizemi ", DEFAULT_ROOM2_TEMP, TEMPSTEP, 1000, MINTEMP, " \xDF""C"},
+  {"Zimni Provoz    ", ON, 1, ON, OFF, ""},
+  {"Max Teplota vody", DEFAULT_MAX_WATER_TEMP, TEMPSTEP, 9000, MINTEMP, " \xDF""C"},
+  {"Provoz Kotle    ", DEFAULT_HEATER, 1, AUTO, OFF, ""},
+  {"Provoz Cerpadla ", DEFAULT_PUMP, 1, AUTO, OFF, ""},
+  {"Hystereze       ", DEFAULT_HYSTEREZE, TEMPSTEP, 300, MINTEMP, " \xDF""C"},
+  {"Zpet <- Sel     ", BLANK, 0, 0, 0, ""}
+};
+
+enum MainMenuItem {
+  ACTROOM1TEMP = 0,
+  ACTROOM1HUM,
+  ACTROOM2TEMP,
+  ACTROOM2HUM,
+  ACTWATER1TEMP,
+  ACTWATER2TEMP, 
+  PUMPON,
+  ELHEATERON,
+  SETTINGS,
+  MAINMENUSIZE
+};
+item_t MAINMENU[MAINMENUSIZE] = {
+  {"Patro Teplota   ", 0, 0, 0, 0, " \xDF""C"},
+  {"Patro Vlhkost   ", 0, 0, 0, 0, " %"},
+  {"Prizemi Teplota ", 0, 0, 0, 0, " \xDF""C"},
+  {"Prizemi Vlhkost ", 0, 0, 0, 0, " %"},
+  {"T vystupni voda ", 0, 0, 0, 0, " \xDF""C"},
+  {"T vstupni  voda ", 0, 0, 0, 0, " \xDF""C"},
+  {"Cerpadlo        ", AUTO, 0, 0, 0, ""},
+  {"Kotel           ", AUTO, 0, 0, 0, ""},
+  {"Nastaveni -> Sel", BLANK, 0, 0, 0, ""}
+};
+
+//LCD pin to Arduino
+const int pin_RS = 8;
+const int pin_EN = 9;
+const int pin_d4 = 4;
+const int pin_d5 = 5;
+const int pin_d6 = 6;
+const int pin_d7 = 7;
+const int pin_BL = 10;
+// LCD Buttons 
+const int NONE = 0;
+const int LEFT = 1;
+const int RIGHT = 2;
+const int UP = 3;
+const int DOWN = 4;
+const int SELECT = 5;
+//Other pins
+const int pin_buzzer = 3; // buzzer PIN
+const int pin_WTS = 15; // Water temperature sensor PIN
+//Room temperature 1 and 2 sensor PIN
+const int pin_RT1 = 16;
+const int pin_RT2 = 17;
+//Relays pins
+const int pin_WP = 18;  // Water pump pin relay
+const int pin_H = 19;   // Heater pin relay
+
+// Default values for the conrol functions
+const unsigned long DEFAULT_HONOFFTIME = 900000;    // Heater can't be turned on earlier than 15 mins after last turn off.
+//const int DEFAULT_HONOFFTIME = 20000;   // test 20 sec. only
+const unsigned long DEFAULT_WPONOFFTIME = 1800000;  // Once the pump is turned on it will run for at least 30 minutes. 
+//const int DEFAULT_WPONOFFTIME = 20000;  // test 20 sec. only
+const int DEFAULT_WTPUMP = 4500;          // When water temp is higher than 45 degrees, the water pump turns on
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(pin_WTS);
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+// Initialize room temperature sensors
+#define DHTTYPE DHT22        // DHT 22  (AM2302)
+DHT dht1(pin_RT1, DHTTYPE);  // Initialize both DHT sensors
+DHT dht2(pin_RT2, DHTTYPE);
+
+// global variables 
+LiquidCrystal lcd(pin_RS, pin_EN, pin_d4, pin_d5, pin_d6, pin_d7);
+MyMenu main_menu(MAINMENUSIZE, MAINMENU);  // menu also stores measured values
+MyMenu settings_menu(SETTINGSMENUSIZE, SETTINGSMENU); // menu also stores configuration values 
+
+/* program start */
+void setup() {
+  // initialize the LCD
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Inicializuji...");
+  // setup the buzzer, pump and heater pins as output
+  pinMode(pin_buzzer, OUTPUT);
+  pinMode(pin_WP, OUTPUT);
+  pinMode(pin_H, OUTPUT);
+  // Turn off both relays and the buzzer
+  stop_alarm();
+  digitalWrite(pin_WP, HIGH);
+  digitalWrite(pin_H, HIGH);
+  // Initiate the temperature libraries
+  sensors.begin();
+  sensors.setWaitForConversion(false);
+  dht1.begin();
+  dht2.begin();
+  read_sensors();  // get the first reading before getting to the control loop to avoid turn on/off at the beginning. 
+  delay(1500);
+  read_sensors();
+  // Read the settings from EEPROM
+  ReadEEPROM();
+}
+
+void loop() {
+  read_sensors();
+  int key = read_key();
+  run_menu(key);
+  control_system();
+}
+
+// Timer class is used to implement delays in some operations. 
+class MyTimer {
+  private:
+    unsigned long start_time;
+    unsigned long duration;
+    bool running;
+
+  public:
+    MyTimer(unsigned long d, bool start = false) {
+      duration = d;
+      if (start) {
+        start_timer();
+      } else
+        running = false;
+    }
+
+    bool expired() {
+      if (millis() - start_time > duration) {
+        running = false;
+        return true;
+      } else
+        return !running;
+    }
+    void start_timer() {
+      start_time = millis();
+      running = true;
+    }
+};
+
+// Heater class is used to provide persistence that allows delays between switch on and off. 
+class MyHeater {
+  private:
+    MyTimer T; // can't be turned on earlier than 15 mins after turn off.
+    bool off;
+
+  public:
+    MyHeater() : T(DEFAULT_HONOFFTIME) {
+      off = true;
+    }
+
+    void heater_on() {
+      if (T.expired()) { 
+        digitalWrite(pin_H, LOW);
+        main_menu.value_set(ELHEATERON, ON);
+        off = false;        
+      }
+    }
+
+    void heater_off() {
+      if (!off) {
+        digitalWrite(pin_H, HIGH);
+        main_menu.value_set(ELHEATERON, OFF);
+        T.start_timer();
+        off = true;
+      }
+    }
+};
+
+// Pump class is used to provide persistence that allows delays between switch on and off. 
+class MyPump {
+  private:
+    MyTimer T;  // Once the pump is turned on it will run for at least 30 minutes. 
+  
+  public:
+    MyPump(unsigned long t = DEFAULT_WPONOFFTIME) : T(t) {
+    }
+    
+    void pump_on() {
+      digitalWrite(pin_WP, LOW);
+      main_menu.value_set(PUMPON, ON);
+      T.start_timer();
+    }
+
+    void pump_off() {
+      if (T.expired()) {
+        digitalWrite(pin_WP, HIGH);
+        main_menu.value_set(PUMPON, OFF);
+      }
+    }
+};
+
+void start_alarm() {
+  digitalWrite(pin_buzzer, LOW);
+}
+
+void stop_alarm() {
+  digitalWrite(pin_buzzer, HIGH);
+}
+
+void ReadEEPROM() {
+  int v;
+
+  EEPROM.get(VPOSITION, v);
+  if (v != VERSION) { // SW version change, force to apply defaults, not what is in the EEPROM. 
+    EEPROM.put(VPOSITION, VERSION); // store new version number.
+    WriteEEPROM(); // take defaults and write them all into EEPROM. This works only beacuse this function is called once, before anything else happens and defaults are in the settings. 
+  }
+  
+  for (int i = 0; i < SETTINGSMENUSIZE - 1; i++) {    
+    EEPROM.get(2 * i, v);
+    if (v <= settings_menu.get_max(i) && v >= settings_menu.get_min(i))
+      settings_menu.value_set(i, v);
+  }
+}
+
+void WriteEEPROM() {
+  for (int i = 0; i < SETTINGSMENUSIZE - 1; i++) 
+    EEPROM.put(2 * i, settings_menu.value_get(i));
+}
+
+int read_key() {
+  // returns key value only after it has been released
+  static int lastkey_pressed = NONE;
+  int r = NONE;
+  unsigned long t;
+  static MyTimer T(333);  // create time for 1/3 second
+
+  int x = analogRead(0);
+
+  if (x < 800 && lastkey_pressed == NONE)
+    T.start_timer();
+
+  if (x < 60)
+    lastkey_pressed = RIGHT;
+  else if (x < 200)
+    lastkey_pressed = UP;
+  else if (x < 400)
+    lastkey_pressed = DOWN;
+  else if (x < 600)
+    lastkey_pressed = LEFT;
+  else if (x < 800)
+    lastkey_pressed = SELECT;
+  else if (lastkey_pressed != NONE) {
+    r = lastkey_pressed;
+    lastkey_pressed = NONE;
+  }
+  if (T.expired()) {
+    r = lastkey_pressed;
+    lastkey_pressed = NONE;
+  }
+
+  return r;
+}
+
+void display_2lines(String Line1, String Line2) {
+  // updates entire display with 2 lines on the input
+  lcd.setCursor(0, 0);
+  lcd.print(Line1);
+  lcd.setCursor(0, 1);
+  lcd.print(Line2);
+}
+
+void run_menu(int key) {
+  static bool menu_type = MAIN_MENU;
+  
+  if (menu_type == MAIN_MENU) {
+    display_2lines(main_menu.text_get(), main_menu.value_getastext());
+    if (key == DOWN || key == RIGHT)
+      main_menu.next();
+    else if (key == UP || key == LEFT)
+      main_menu.prev();
+    else if (key == SELECT && main_menu.last()) { // selecting last mainu menu item means moving to configuration menu
+      menu_type = SETTINGS_MENU;
+      main_menu.beginning();
+    }
+  } else if (menu_type == SETTINGS_MENU) {
+    display_2lines(settings_menu.text_get(), settings_menu.value_getastext());
+    switch (key) {
+      case DOWN:
+        settings_menu.next();
+        break;
+      case UP:
+        settings_menu.prev();
+        break;
+      case LEFT:
+        if (!settings_menu.last()) // selecting last menu item of configuration menu means going back to main menu
+          settings_menu.value_decrease(); 
+        break;  
+      case RIGHT:
+        if (!settings_menu.last()) 
+          settings_menu.value_increase();
+        break;  
+      case SELECT:
+        menu_type = MAIN_MENU;
+        settings_menu.beginning();
+        WriteEEPROM();
+    }
+  }
+}
+
+void read_sensors() {
+  // read water temperature and room temperature every 1 second
+  static MyTimer T(1000, true);  // create a timer for 1 second and start it immediately
+  
+  if (T.expired()) {
+    // Read water 1 temperature
+   // sensors.setWaitForConversion(false);
+    sensors.requestTemperatures();
+    float Water1T = sensors.getTempCByIndex(1); // index to be tried and set when sensor changes
+    if (Water1T != DEVICE_DISCONNECTED_C)
+      main_menu.value_set(ACTWATER1TEMP, (int)(100 * Water1T));
+    else
+      main_menu.value_set(ACTWATER1TEMP, ERROR);  // Error value
+    // Read water 2 temperature
+    float Water2T = sensors.getTempCByIndex(0); // index to be tried and set when sensor changes
+    if (Water2T != DEVICE_DISCONNECTED_C)
+      main_menu.value_set(ACTWATER2TEMP, (int)(100 * Water2T));
+    else
+      main_menu.value_set(ACTWATER2TEMP,  ERROR);  // Error value
+
+    // Read Rooms temperature and humidity
+    float Room1H = dht1.readHumidity();
+    float Room1T = dht1.readTemperature();
+    if (isnan(Room1T))
+      main_menu.value_set(ACTROOM1TEMP, ERROR);  // Error value
+    else
+      main_menu.value_set(ACTROOM1TEMP, (int)(100 * Room1T));
+    if (isnan(Room1H))
+      main_menu.value_set(ACTROOM1HUM, ERROR);  // Error value
+    else
+      main_menu.value_set(ACTROOM1HUM, (int)(100 * Room1H));
+    
+    float Room2H = dht2.readHumidity();
+    float Room2T = dht2.readTemperature();
+    if (isnan(Room2T))
+      main_menu.value_set(ACTROOM2TEMP, ERROR);  // Error value
+    else
+      main_menu.value_set(ACTROOM2TEMP, (int)(100 * Room2T));
+    if (isnan(Room2H))
+      main_menu.value_set(ACTROOM2HUM, ERROR);  // Error value
+    else
+      main_menu.value_set(ACTROOM2HUM, (int)(100 * Room2H));
+
+    T.start_timer();  // start the timer again
+  }
+}
+
+void control_system() {
+  static MyHeater HT;
+  static MyPump P;
+  float hystereze = settings_menu.value_get(SETHYSTER);  //  up and down difference. Mozna by melo byt ve stupnich, nikoliv procentech.
+
+  // Any water temp > Max water temp => run Alarm, else stop Alarm. This is to avoid boiling the water out of stove. 
+  int t = max(main_menu.value_get(ACTWATER1TEMP),main_menu.value_get(ACTWATER2TEMP));
+  if (t >= settings_menu.value_get(SETMAXWATERTEMP))
+    start_alarm();
+  else if (main_menu.value_get(ACTWATER1TEMP) < settings_menu.value_get(SETMAXWATERTEMP))
+    stop_alarm();
+  
+  // Electric Heater ON, OFF, or AUTO. If Auto then thermostat is on, i.e. Room temp to match preset temp.
+  // Master sensor is either ROOM #1 or ROOM #2 (with plumbing that must not freeze). Depending on master, either preset #1 or #2 is used as required temperature. 
+  int master_sensor = ACTROOM1TEMP;
+  int master_temp = SETROOM1TEMP;
+  if (settings_menu.value_get(SETANTIFREEZE) == ON) {
+    master_sensor = ACTROOM2TEMP;
+    master_temp = SETROOM2TEMP;
+  }
+  switch (settings_menu.value_get(SETELHEATER)) {
+    case ON:
+      HT.heater_on();
+      break;      
+    case OFF:
+      HT.heater_off();
+      break;
+    case AUTO:
+      if (main_menu.value_get(master_sensor) > settings_menu.value_get(master_temp) + hystereze)
+        HT.heater_off();
+      else if (main_menu.value_get(master_sensor) < settings_menu.value_get(master_temp) - hystereze)
+        HT.heater_on();
+  }
+  // Electric Pump ON, OFF, or AUTO. If Auto then pump is on if water temperature is higher then threshold. 
+  switch (settings_menu.value_get(SETPUMP)) {
+    case ON:
+      P.pump_on();
+      break;      
+    case OFF:
+      P.pump_off();
+      break;
+    case AUTO:
+      // Turn on pump if water is over preset temp.
+      if (main_menu.value_get(ACTWATER1TEMP) > DEFAULT_WTPUMP)
+        P.pump_on();
+      else
+        P.pump_off();
+  }
+}
